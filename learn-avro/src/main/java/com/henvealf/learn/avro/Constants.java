1cacc3f3cfd1d0df38c832b69ab9191b6a2ccfa3
Henvealf <1103693087@qq.com>
2018-08-04 19:39:53 +0800

    [add] avro learn

---
+++ learn-avro/src/main/java/com/henvealf/learn/avro/Constants.java
@@ -0,0 +1,10 @@
+package com.henvealf.learn.avro;
+
+import java.io.File;
+
+/**
+ * @author hongliang.yin/Henvealf on 2018/8/4
+ */
+public class Constants {
+    public static final String USER_AVRO_FILE = "learn-avro" + File.separatorChar +"users.avro";
+}
