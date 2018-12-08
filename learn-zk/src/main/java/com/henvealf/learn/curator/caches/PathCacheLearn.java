b0ba90e52494df39c6a8036f442226f65089e4b0
henvealf <1103693087@qq.com>
2018-12-08 18:01:26 +0800

    curator recipes cache path

--- learn-zk/src/main/java/com/henvealf/learn/curator/caches/PathCacheLearn.java
+++ learn-zk/src/main/java/com/henvealf/learn/curator/caches/PathCacheLearn.java
@@ -1,16 +1,10 @@
 package com.henvealf.learn.curator.caches;
 
 import com.henvealf.learn.curator.CuratorConstants;
-import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.cache.*;
 import org.apache.curator.retry.ExponentialBackoffRetry;
-import org.apache.log4j.net.SyslogAppender;
-import org.junit.Test;
-
-import java.lang.reflect.ParameterizedType;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
@@ -28,49 +22,34 @@ public class PathCacheLearn {
 
     public static void main(String[] args) throws Exception {
 
+        // 设置客户端
         CuratorFramework cf = CuratorFrameworkFactory.newClient(CuratorConstants.ZK_URL, new ExponentialBackoffRetry(1000, 3));
         cf.start();
 
+        // 创建测试目录
         if (cf.checkExists().forPath(PATH) == null) {
             cf.create().creatingParentsIfNeeded().forPath(PATH);
 
-//            for (int i = 0; i < 3; i ++) {
-//                cf.create().forPath(PATH + "/cache-"+i, (" i ma cache data " + i).getBytes(StandardCharsets.UTF_8));
-//            }
+            for (int i = 0; i < 3; i ++) {
+                cf.create().forPath(PATH + "/cache-"+i, (" i ma cache data " + i).getBytes(StandardCharsets.UTF_8));
+            }
 
         }
 
-
+        // 设置 PathChildrenCache 实例
         PathChildrenCache pathChildrenCache = new PathChildrenCache(cf, PATH, true);
 
-
+        // 为缓存添加事件监听器
         pathChildrenCache.getListenable().addListener((curatorFramework, pathChildrenCacheEvent) -> {
             switch (pathChildrenCacheEvent.getType()) {
                 case INITIALIZED:
                     System.out.println(" path cache have INITIALIZED event");
-
-                    List<ChildData> currentDatas = pathChildrenCache.getCurrentData();
-//
-//                    for (ChildData childData: currentDatas) {
-//                        System.out.println("childData.getPath(): " + childData.getPath());
-//                        System.out.println("childData.getData(): " + new String(childData.getData()));
-//                        System.out.println("childData.getStat(): " + childData.getStat().toString());
-//                    }
-                    System.out.println("child size in INITIALIZED listener: " + currentDatas.size());
-
+                    printCurrentData(pathChildrenCache, "INITIALIZED listener");
 
                     break;
                 case CHILD_ADDED:
                     System.out.println(" path child has add");
-
-                    currentDatas = pathChildrenCache.getCurrentData();
-//
-//                    for (ChildData childData: currentDatas) {
-//                        System.out.println("childData.getPath(): " + childData.getPath());
-//                        System.out.println("childData.getData(): " + new String(childData.getData()));
-//                        System.out.println("childData.getStat(): " + childData.getStat().toString());
-//                    }
-                    System.out.println("child size in CHILD_ADDED listener: " + currentDatas.size());
+                    printCurrentData(pathChildrenCache, "CHILD_ADDED listener");
 
                     break;
                 case CHILD_REMOVED:
@@ -82,9 +61,28 @@ public class PathCacheLearn {
             }
         });
 
+        // start 可以设置模式
         pathChildrenCache.start(PathChildrenCache.StartMode.NORMAL);
 
+        printCurrentData(pathChildrenCache, "");
 
+
+        // 开始 cache 并手动添加新的节点。
+        // System.out.println("begin add..");
+        // cf.create().forPath(PATH + "/cache-" + System.currentTimeMillis(), ("i am new " + System.currentTimeMillis()).getBytes());
+        // System.out.println("end add..");
+
+        System.out.println("begin sleep..");
+        Thread.sleep(1000);
+
+        pathChildrenCache.close();
+        cf.close();
+
+    }
+
+    public static void printCurrentData(PathChildrenCache pathChildrenCache, String flag) {
+
+        System.out.printf("--------- " + flag + "--------------");
         List<ChildData> currentDatas = pathChildrenCache.getCurrentData();
 
         for (ChildData childData: currentDatas) {
@@ -92,19 +90,8 @@ public class PathCacheLearn {
             System.out.println("childData.getData(): " + new String(childData.getData()));
             System.out.println("childData.getStat(): " + childData.getStat().toString());
         }
-        System.out.println("child size: " + currentDatas.size());
 
-        // 开始cache 并手动添加新的节点。
-
-        System.out.println("begin sleep..");
-//        System.out.println("begin add..");
-//        cf.create().forPath(PATH + "/cache-" + System.currentTimeMillis(), ("i am new " + System.currentTimeMillis()).getBytes());
-//        System.out.println("end add..");
-        Thread.sleep(1000);
-
-        pathChildrenCache.close();
-        cf.close();
-
+        System.out.println("Child size in " + flag + ": "+ currentDatas.size());
     }
 
 }
