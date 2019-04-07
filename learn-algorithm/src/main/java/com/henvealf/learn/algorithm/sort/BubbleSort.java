2f3a4cb91aae52a0b40ebbe91e940fa5383e70ef
henvealf <1103693087@qq.com>
2019-04-07 23:52:32 +0800

    add sort

---
+++ learn-algorithm/src/main/java/com/henvealf/learn/algorithm/sort/BubbleSort.java
@@ -0,0 +1,31 @@
+package com.henvealf.learn.algorithm.sort;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+/**
+ * <p>
+ * 冒泡排序。
+ * 时间复杂度 O(N方)
+ * <p>
+ *
+ * @author hongliang.yin/Henvealf on 2019-04-06
+ */
+public class BubbleSort implements Sortor{
+
+    public void sort(int[] nums) {
+
+        for (int i = 0; i < nums.length - 1; i ++) {
+            for (int j = 0; j < nums.length - 1 - i; j ++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+}
