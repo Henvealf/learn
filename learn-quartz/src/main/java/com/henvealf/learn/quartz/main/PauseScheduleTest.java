44194a24b6639fcf649c4a0179e3981fb9558ce0
henvealf <1103693087@qq.com>
2019-09-03 10:25:45 +0800

    quartz: test pause schedule

--- learn-quartz/src/main/java/com/henvealf/learn/quartz/main/PauseScheduleTest.java
+++ learn-quartz/src/main/java/com/henvealf/learn/quartz/main/PauseScheduleTest.java
@@ -9,14 +9,14 @@ import static org.quartz.TriggerBuilder.newTrigger;
 
 /**
  * 测试暂停任务。
- *
+ * 暂停会停止调度任务。
+ * 算是轻量级的 unschedule
  * @author hongliang.yin/Henvealf
  * @date 2019-08-29
  */
 public class PauseScheduleTest {
 
     public static void main(String[] args) throws SchedulerException, InterruptedException {
-
 
         Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
         // and start it off
@@ -40,17 +40,17 @@ public class PauseScheduleTest {
         // Tell quartz to schedule the job using our trigger
         scheduler.scheduleJob(job, trigger);
         Thread.sleep(2000);
-        System.out.println("unscheduled");
-        scheduler.unscheduleJob(trigger.getKey());
+        System.out.println("Begin paused");
+        scheduler.pauseAll();
         while (!scheduler.getCurrentlyExecutingJobs().isEmpty()) {
             System.out.println("Now there is some jobs on the schedule, wait 1 second");
             Thread.sleep(500);
         }
-        System.out.println("All job is stopped");
-        Thread.sleep(1000);
-        System.out.println("ReBegin start job");
-        scheduler.scheduleJob(job, trigger);
-        Thread.sleep(2000);
+        System.out.println("All job is paused");
+        Thread.sleep(4000);
+        System.out.println("resume all job");
+        scheduler.resumeAll();
+        Thread.sleep(20000);
         System.out.println("Begin shutdown scheduler");
         scheduler.shutdown();
         System.out.println("Finish shutdown scheduler, wait schedule thead end");
