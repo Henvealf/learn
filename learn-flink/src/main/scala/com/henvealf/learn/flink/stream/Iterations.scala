eda3200bfe1975f848762a7651e46d98f05355d7
henvealf <1103693087@qq.com>
2019-09-25 19:48:41 +0800

    Learn Flink, Add UsingValueState

--- learn-flink/src/main/scala/com/henvealf/learn/flink/stream/Iterations.scala
+++ learn-flink/src/main/scala/com/henvealf/learn/flink/stream/Iterations.scala
@@ -1,8 +1,9 @@
 package com.henvealf.learn.flink.stream
 
 import org.apache.flink.api.scala._
+import org.apache.flink.streaming.api.datastream.DataStreamUtils
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-
+import scala.collection.JavaConverters.asScalaIteratorConverter
 /**
   * 不清楚是啥意思
   * iteration 的使用
@@ -11,20 +12,16 @@ import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
   */
 object Iterations {
   def main(args: Array[String]): Unit = {
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
-    val executionConfig = env.getConfig
+    val env = StreamExecutionEnvironment.createLocalEnvironment()
 
     val someIntegers = env.generateSequence(0, 10)
     val iteratedIntegers = someIntegers.iterate(
       iteration => {
       val minusOne = iteration.map( v => v - 1)
-      val stillGreaterThanZero = minusOne.filter (_ > 0)
-      val lessThanZero = minusOne.filter(_ >= 5)
-      (stillGreaterThanZero, lessThanZero)
+      (minusOne.filter (_ > 5), minusOne.filter(_ < 5))
       }
     )
-    iteratedIntegers.print()
-    env.execute()
-
+    val result = DataStreamUtils.collect(iteratedIntegers.javaStream).asScala
+    result.foreach(str => println("result--------:" + str))
   }
 }
