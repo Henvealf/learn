f28c3743b4b869b028ea759468cce71a7c74e76f
henvealf <1103693087@qq.com>
2019-12-29 21:10:38 +0800

    [GO] basic, flowcontrol, moretypes

---
+++ learn-go/basic/14_类型推导.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42 // 修改这里！
+	fmt.Printf("v is of type %T\n", v)
+	v1 := 12.3
+	fmt.Printf("v1 is of type %T\n", v1)
+	v2 := 0.867 + 0.5i
+	fmt.Printf("v1 is of type %T\n", v2)
+}
+
